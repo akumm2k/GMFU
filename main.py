f6d8ba52820c9eb149701870e55ff24e2817560e
akumm2k <82381989+akumm2k@users.noreply.github.com>
2022-09-22 17:07:42 +0200

    Handle script execution for non-macOS operating systems

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
+from sys import platform
 from src.make_and_move import make_and_move
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 2, 'why so many?'
+    if platform == 'darwin':
+        assert len(sys.argv) == 2, 'why so many?'
 
-    url = sys.argv[-1]
-    print(sys.argv)
-    make_and_move(url)
+        url = sys.argv[-1]
+        print(sys.argv)
+        make_and_move(url)
+    else:
+        print('this tool requires macOS')
